b08e81ebf73d69df0ff8f08744e9cb3f526e2783
grs-se <george_ricesmith@hotmail.com>
2023-04-04 19:49:32 +0100

    Testing continuous deployment

--- components/common/navigation/MainNav/index.vue
+++ components/common/navigation/MainNav/index.vue
@@ -15,7 +15,7 @@ const navHeightClass = ref('h-16');
 
 <template>
   <nav :class="['w-full', 'text-sm', 'font-normal', navHeightClass]">
-    <div class="fixed top-0 left-0 z-50 h-16 w-full bg-red-500">
+    <div class="fixed top-0 left-0 z-50 h-16 w-full bg-white">
       <div
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
