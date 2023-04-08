b997feab3fee9cce406b3c2f0816c18f73cda559
grs-se <george_ricesmith@hotmail.com>
2023-04-08 22:09:25 +0100

    Router behavior scrollToTop working

---
+++ app/router.options.ts
@@ -0,0 +1,22 @@
+import type { RouterConfig } from '@nuxt/schema';
+
+export default <RouterConfig>{
+  scrollBehavior(to, _from, savedPosition) {
+    return new Promise((resolve, _reject) => {
+      setTimeout(() => {
+        if (savedPosition) {
+          resolve(savedPosition);
+        } else {
+          if (to.hash) {
+            resolve({
+              el: to.hash,
+              top: 80,
+            });
+          } else {
+            resolve({ top: 0 });
+          }
+        }
+      }, 100);
+    });
+  },
+};
--- middleware/route.global.ts
+++ middleware/route.global.ts
@@ -1,5 +1,15 @@
+// export default defineNuxtRouteMiddleware((to, from) => {
+//   if (to.path !== from.path && process.client) {
+//     window.scrollTo(0, 0);
+//   }
+// });
+
 export default defineNuxtRouteMiddleware((to, from) => {
-  if (to.path !== from.path && process.client) {
-    window.scrollTo(0, 0);
-  }
+  useNuxtApp().hook('page:finish', () => {
+    if (history.state.scroll) {
+      setTimeout(() => window.scrollTo(history.state.scroll), 0);
+    } else {
+      setTimeout(() => window.scrollTo(0, 0), 0);
+    }
+  });
 });
